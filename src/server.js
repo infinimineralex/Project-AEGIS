4ab238bcd739acfc2de208298f6140c54ef89371
infinimineralex <ivoRobotnik36@gmail.com>
2025-02-05 18:53:52 -0800

    updated formatting

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Routes (once implemented)
 app.use('/api/auth', authRoutes);
 app.use('/api/passwords', passwordRoutes);
 
