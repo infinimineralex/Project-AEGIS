5f872c566c76b2ff8598659c8b40b6725a01d63c
infinimineralex <ivoRobotnik36@gmail.com>
2025-03-27 16:54:59 -0700

    backend prep

--- src/server.js
+++ src/server.js
@@ -20,7 +20,11 @@ app.get('/', (req, res) => {
     res.send('Aegis Password Manager Backend');
 });
 
-// Start Server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Only listen when not on Vercel
+if (process.env.VERCEL_ENV !== 'production') {
+  app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
