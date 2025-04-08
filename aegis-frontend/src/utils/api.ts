ae96b78c91daa122d35ceb4cbf7916001a157af2
infinimineralex <ivoRobotnik36@gmail.com>
2025-04-08 11:25:29 -0700

    updated base url

--- aegis-frontend/src/utils/api.ts
+++ aegis-frontend/src/utils/api.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 //const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || '';
 
 const api = axios.create({
-  //baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
+  //baseURL: 'http://localhost:5000',
 });
 
 // Attach the API key to every request
