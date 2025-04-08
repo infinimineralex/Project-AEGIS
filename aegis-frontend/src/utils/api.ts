cafe5051dfa02aeb6b9493e2f90c73415ef43783
infinimineralex <ivoRobotnik36@gmail.com>
2025-04-08 11:38:19 -0700

    changed api.ts

--- aegis-frontend/src/utils/api.ts
+++ aegis-frontend/src/utils/api.ts
@@ -1,17 +1,7 @@
 import axios from 'axios';
 
-//const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || '';
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
-  //baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
-// Attach the API key to every request
-/*api.interceptors.request.use((config) => {
-  config.headers = config.headers || {};
-  config.headers['apikey'] = SUPABASE_ANON_KEY;
-  return config;
-});*/
 
 export default api;
